d8b70473bda3f129843e15f8e55e724f54916f56
bamanzi <none@none>
2013-06-22 11:41:00 +0800

    add & configure package rinari, rhtml & yaml-mode

---
+++ _init-rails.el
@@ -0,0 +1,36 @@
+;;* ruby on rails
+;;** rhtml-mode for editing html template
+(autoload 'rhtml-mode "rhtml-mode"
+  "Embedded Ruby Mode (RHTML)" t)
+
+;;DONE: web-mode is better
+;;(add-to-list 'auto-mode-alist '("\\.rhtml\\'" . rhtml-mode))
+;;(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+
+
+;;** rinari: rails code navigation & utils
+;;http://rinari.rubyforge.org/
+(autoload 'rinari-launch "rinari"
+  "Call function `rinari-minor-mode' if inside a rails project." t)
+
+(eval-after-load "ruby-mode"
+  `(progn
+     (add-hook 'ruby-mode-hook 'rinari-launch)))
+
+(eval-after-load "web-mode"
+  `(progn
+     (add-hook 'web-mode-hook 'rinari-launch)))
+
+(eval-after-load "rhtml-mode"
+  `(progn
+     (add-hook 'rhtml-mode-hook 'rinari-launch)))
+
+(eval-after-load "yaml-mode"
+  `(progn
+     (add-hook 'yaml-mode-hook 'rinari-launch)))
+
+;;** yaml-mode
+(autoload 'yaml-mode  "yaml-mode"
+  "Simple mode to edit YAML." t)
+
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
